cb1fe0c082a953ff90c0e0952566009a45c2fcfe
Yasuhiro Inami <inamiy@gmail.com>
2018-12-17 01:18:19 +0900

    [swift-rewriter] Update

--- Formula/swift-rewriter.rb
+++ Formula/swift-rewriter.rb
@@ -3,10 +3,16 @@ class SwiftRewriter < Formula
   homepage "https://github.com/inamiy/SwiftRewriter"
   url "https://github.com/inamiy/SwiftRewriter.git",
       :tag => "0.1.0",
-      :revision => "7bb35b49681f43154863c82ad5c38c44e33b0602"
+      :revision => "dc25281fe1ba8310743790d6c3473fe519de38bc"
   head "https://github.com/inamiy/SwiftRewriter.git"
 
   depends_on :xcode => ["10.0", :build]
+
+  bottle do
+    root_url "https://github.com/inamiy/SwiftRewriter/releases/download/0.1.0/"
+    cellar :any
+    sha256 "c6f6257cdea9b14f4c17a752a1e725148badb3951511df0d2914f52ed8fc4916" => :mojave
+  end
 
   def install
     system "make", "install", "prefix=#{prefix}"
